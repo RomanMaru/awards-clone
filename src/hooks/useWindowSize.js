fa9e28b83a75db2722860738fd814f4dbd2c1bb3
Roman Maru <romanmaru10@gmail.com>
2020-08-11 04:18:56 -0700

    add if statement for window object

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,23 +1,28 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
+if (typeof window !== 'undefined') {
+
+}
 export default function useWindowSize() {
-  function getSize() {
-    return {
-      width: window.innerWidth,
-      height: window.innerHeight
+
+  if (typeof window !== 'undefined') {
+    function getSize() {
+      return {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
     }
+
+    const [windowSize, setWindowSize] = useState(getSize)
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize(getSize())
+      }
+
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    return windowSize
   }
-
-  const [windowSize, setWindowSize] = useState(getSize)
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize(getSize())
-    }
-
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
-  return windowSize
 }
