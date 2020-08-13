752a89ae8897b930c0d2c7f23048f74756b57cca
Roman Maru <romanmaru10@gmail.com>
2020-08-12 18:31:29 -0700

    try if statement

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,24 +1,27 @@
 import { useState, useEffect } from 'react'
-import {withWindowSize} from 'react-fns'
 
-function useWindowSize({width, height}) {
-  function getSize() {
-    return {
-      width: width,
-      height: height
+
+function useWindowSize({ width, height }) {
+  if (typeof window !== 'undefined') {
+    function getSize() {
+      return {
+        width: width,
+        height: height
+      }
     }
+    const [windowSize, setWindowSize] = useState(getSize)
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize(getSize())
+      }
+
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    })
+    return windowSize
   }
-  const [windowSize, setWindowSize] = useState(getSize)
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize(getSize())
-    }
-
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  })
-  return windowSize
 }
 
-export default withWindowSize(useWindowSize)
+export default useWindowSize
